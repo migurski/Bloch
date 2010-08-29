351e413a617b71b6b37590554b6da8f5eb6be243
migurski <migurski@ubuntu.(none)>
2010-08-28 20:11:33 -0700

    Writing out to shapefile and added join_multiline_parts() to coalesce atomized linestrings

--- build.py
+++ build.py
@@ -1,6 +1,7 @@
 from sys import argv, exit
 from osgeo import ogr
 from shapely.wkb import loads, dumps
+from shapely.geometry import LineString, MultiLineString
 from itertools import combinations
 
 class Field:
@@ -14,11 +15,11 @@ class Field:
 class Datasource:
     """
     """
-    def __init__(self, srs, geom_type, fields, features, shapes):
+    def __init__(self, srs, geom_type, fields, values, shapes):
         self.srs = srs
         self.fields = fields
         self.geom_type = geom_type
-        self.features = features
+        self.values = values
         self.shapes = shapes
 
 def load_datasource(filename):
@@ -35,26 +36,43 @@ def load_datasource(filename):
               for field_defn 
               in [layer_defn.GetFieldDefn(i) for i in range(layer_defn.GetFieldCount())]]
 
-    features, shapes = [], []
+    values, shapes = [], []
     
     for feature in layer:
-        values = [feature.GetField(field.name) for field in fields]
-        features.append(values)
-    
-        shape = loads(feature.geometry().ExportToWkb())
-        shapes.append(shape)
+        values.append([feature.GetField(field.name) for field in fields])
+        shapes.append(loads(feature.geometry().ExportToWkb()))
 
-    return Datasource(srs, geom_type, fields, features, shapes)
+    return Datasource(srs, geom_type, fields, values, shapes)
+
+def join_multiline_parts(shape):
+    """
+    """
+    if shape.geom_type != 'MultiLineString':
+        return shape
+    
+    lines = [LineString(list(geom.coords)) for geom in shape.geoms]
+    
+    i = 0
+    
+    while i < len(lines) - 1:
+        if lines[i].touches(lines[i + 1]):
+            c1, c2 = list(lines[i].coords), list(lines.pop(i + 1).coords)
+            lines[i] = LineString(c1 + c2[1:])
+
+        else:
+            i += 1
+
+    return MultiLineString([list(line.coords) for line in lines])
 
 datasource = load_datasource(argv[1])
-indexes = range(len(datasource.features))
+indexes = range(len(datasource.values))
 
 shared = {}
 
 for (i, j) in combinations(indexes, 2):
 
-    feature1 = datasource.features[i]
-    feature2 = datasource.features[j]
+    feature1 = datasource.values[i]
+    feature2 = datasource.values[j]
     
     shape1 = datasource.shapes[i]
     shape2 = datasource.shapes[j]
@@ -62,7 +80,7 @@ for (i, j) in combinations(indexes, 2):
     if shape1.intersects(shape2):
         print feature1[4], 'and', feature2[4],
         
-        border = shape1.intersection(shape2)
+        border = join_multiline_parts(shape1.intersection(shape2))
         shared[(i, j)] = border
         
         print '-', border.geom_type, int(border.length)
@@ -83,7 +101,7 @@ for i in indexes:
 
     shared_lengths = [border.length for (key, border) in shared.items() if i in key]
     
-    print datasource.features[i][4],
+    print datasource.values[i][4],
     print datasource.shapes[i].geom_type, int(datasource.shapes[i].length),
     print '=', unshared[i].geom_type, int(unshared[i].length),
     print '+', map(int, shared_lengths),
@@ -94,20 +112,27 @@ for i in indexes:
 
 driver = ogr.GetDriverByName('ESRI Shapefile')
 source = driver.CreateDataSource('out.shp')
-newlayer = source.CreateLayer('default', datasource.srs, datasource.geom_type)
+newlayer = source.CreateLayer('default', datasource.srs, ogr.wkbLineString)
 
 for field in datasource.fields:
     field_defn = ogr.FieldDefn(field.name, field.type)
     field_defn.SetWidth(field.width)
     newlayer.CreateField(field_defn)
 
-feat = ogr.Feature(newlayer.GetLayerDefn())
+for (i, shape) in enumerate(unshared):
+    if shape.geom_type not in ('LineString', 'MultiLineString'):
+        continue
 
-for (i, field) in enumerate(datasource.fields):
-    feat.SetField(field.name, datasource.features[0][i])
+    feat = ogr.Feature(newlayer.GetLayerDefn())
+    
+    for (j, field) in enumerate(datasource.fields):
+        feat.SetField(field.name, datasource.values[i][j])
+    
+    geom = ogr.CreateGeometryFromWkb(dumps(shape))
+    
+    feat.SetGeometry(geom)
 
-geom = ogr.CreateGeometryFromWkb(dumps(datasource.shapes[0]))
+    newlayer.CreateFeature(feat)
 
-feat.SetGeometry(geom)
-
-newlayer.CreateFeature(feat)
+    if shape.geom_type == 'MultiLineString':
+        print len(shape.geoms), datasource.values[i][4]
